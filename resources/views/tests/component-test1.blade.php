6a3885c9ffc13bae28ce031b32bc54c2b6fedbff
Ryo Takamatsu <ryo2267089@gmail.com>
2022-05-19 22:04:14 +0900

    属性で設定

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,3 +1,6 @@
 <x-tests.app>
+    <x-slot name="header">Header1</x-slot>
 component1
+
+    <x-tests.card title="タイトル" content="コンテンツ"/>
 </x-tests.app>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,4 @@
 <x-tests.app>
+<x-slot name="header">Header2</x-slot>
 component2
 </x-tests.app>
