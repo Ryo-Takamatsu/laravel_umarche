73c244d04ab20606fad0813124c419bc0ad419e6
Ryo Takamatsu <ryo2267089@gmail.com>
2022-05-21 14:12:41 +0900

    componentAttributeBag

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,7 +1,6 @@
 <x-tests.app>
     <x-slot name="header">Header1</x-slot>
-component1
-
     <x-tests.card title="タイトル" content="コンテンツ" :message="$message"/>
     <x-tests.card title="タイトル" />
+    <x-tests.card title="CSSタイトルを変更したい" class="bg-red-300"/>
 </x-tests.app>
