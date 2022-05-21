e22cfae5d6e5597e2b08a931c70c2013d58d1893
Ryo Takamatsu <ryo2267089@gmail.com>
2022-05-21 13:27:41 +0900

    変数

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,5 +2,5 @@
     <x-slot name="header">Header1</x-slot>
 component1
 
-    <x-tests.card title="タイトル" content="コンテンツ"/>
+    <x-tests.card title="タイトル" content="コンテンツ" :message="$message"/>
 </x-tests.app>
