5e0af6861976e0e508a6e3fd857be2572bb8b903
Ryo Takamatsu <ryo2267089@gmail.com>
2022-05-22 00:00:56 +0900

    serviceProviderTest

--- app/Http/Controllers/LifeCicleTestController.php
+++ app/Http/Controllers/LifeCicleTestController.php
@@ -6,6 +6,18 @@ use Illuminate\Http\Request;
 
 class LifeCicleTestController extends Controller
 {
+
+    public function showServiceProviderTest() {
+
+        $encrypt = app()->make('encrypter');
+        $password = $encrypt->encrypt('password');
+
+        $sample = app()->make('serviceProviderTest');
+
+        dd($sample, $password, $encrypt->decrypt($password));
+
+    }
+
     public function showServiceContainerTest() {
         app()->bind('lifeCicleTest', function(){
             return 'ライフサイクルテスト';
